0e1c24014202e9b7ca6762a60b555fe06a928399
Fabrício Sousa <fabrjcio@gmail.com>
2022-12-16 23:55:36 -0300

    my neovim cfg

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,41 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "sumneko_lua",
+                       "html",
+                       "quick_lint_js",
+                       "tsserver",
+                       "jedi_language_server",
+                       "tailwindcss",
+                       "dockerls",
+                       "eslint",
+                       "emmet_ls"
+                     }
+})
+
+local on_attach = function(_, _)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+require("lspconfig").sumneko_lua.setup {
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+    },
+  }
+}
+
---
+++ lua/core/plugin_config/nightfox.lua
@@ -0,0 +1,8 @@
+require('nightfox').setup({
+  options = {
+    bg1 = "#0b1012"
+  }
+
+})
+-- setup must be called before loading
+vim.cmd("colorscheme nightfox")
